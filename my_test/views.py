ac7c4033c95a34555c289f06c2d20c938f6d14c9
prasantapy <prasanta.sethi.py@gmail.com>
2025-07-12 17:39:13 +0530

    first commit

---
+++ my_test/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def emp_details(req):
+    emp_data={
+        'eid':101,
+        'ename':'sunny',
+        'esal':1000,
+        'dept':10
+    }
+    res='<h1>employe id : {}<br>employee name : {}<br>emplayee sal : {}<br> emplayee deptno: {} </h1>'.format(emp_data['eid'],emp_data['ename'],emp_data['esal'],emp_data['dept'],emp_data['eid'])
+    return HttpResponse(res)
+import json
+def emp_details_json_format(req):
+    emp_data={
+
+        'eid':101,
+        'ename':'sunny',
+        'esal':1000,
+        'dept':10
+    }
+    json_data=json.dumps(emp_data)
+    print(type(json_data))
+    return HttpResponse(json_data,content_type='application/json')
